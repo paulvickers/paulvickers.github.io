12b30aebc7a59578e3c7ff2de5681f9a90ea7be7
Paul Vickers <paul.vickers@northumbria.ac.uk>
2018-06-12 12:08:27 +0100

    Build site at 2018-06-12 11:08:27 UTC

--- Gemfile
+++ Gemfile
@@ -27,4 +27,5 @@ group :jekyll_plugins do
   gem "jekyll-gist"
   gem "jekyll-feed"
   gem "jemoji"
+  gem "reduce"
 end
--- vendor/bundle/ruby/2.1.0/gems/json-1.8.3/ext/json/ext/generator/siteconf20161028-68453-16pmxm3.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20161028-68453-1h1u0d"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
